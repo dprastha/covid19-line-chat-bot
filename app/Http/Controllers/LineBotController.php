626d12af2d5604247ebf1baebabcee52eee56dd6
dprastha <danielprastha4@gmail.com>
2021-11-08 14:00:11 +0700

    fix service location

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 
-use App\Http\Services\LineBotService;
+use App\Services\LineBotService;
 use Illuminate\Http\Request;
 
 class LineBotController extends Controller
--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Services;
+namespace App\Services;
 
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
