299f257defeabfb959f6b52fac9e3b0ef8e66df2
dprastha <danielprastha4@gmail.com>
2021-11-09 11:25:50 +0700

    add covid service

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\LineBotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Line Webhook
 Route::post('/webhook', [LineBotController::class, 'webhook']);
+
+Route::get('data-covid', [LineBotController::class, 'getDataCovid']);
