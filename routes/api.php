c48db9bf74955b21cfa5b8c240aefa882a8e4371
dprastha <danielprastha4@gmail.com>
2021-11-09 08:41:22 +0700

    delete unused route

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,4 @@ use App\Http\Controllers\LineBotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/get-message', function (Request $request) {
-
-    logger("message request : ", $request->all());
-});
-
-Route::post('/get-message', [LineBotController::class, 'getMessage']);
-
 Route::post('/webhook', [LineBotController::class, 'webhook']);
